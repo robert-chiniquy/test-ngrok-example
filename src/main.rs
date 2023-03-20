a1c8a73d099ace66428f37be3d4964e0a2b28323
Robert Chiniquy <robert.chiniquy@gmail.com>
2023-03-20 13:14:19 -0700

    the trait bound for<'a> std::net::SocketAddr: axum::extract::connect_info::Connected<&'a ngrok::Conn> is not satisfied

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::net::SocketAddr;
+
+use axum::{extract::ConnectInfo, routing::get, Router};
+use ngrok::prelude::*;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // build our application with a single route
+    let app = Router::new().route(
+        "/",
+        get(
+            |ConnectInfo(remote_addr): ConnectInfo<SocketAddr>| async move {
+                format!("Hello, {remote_addr:?}!\r\n")
+            },
+        ),
+    );
+
+    let tun = ngrok::Session::builder()
+        // Read the token from the NGROK_AUTHTOKEN environment variable
+        .authtoken_from_env()
+        // Connect the ngrok session
+        .connect()
+        .await?
+        // Start a tunnel with an HTTP edge
+        .http_endpoint()
+        .listen()
+        .await?;
+
+    // println!("Tunnel started on URL: {:?}", tun.url());
+
+    // // Instead of binding a local port like so:
+    // // axum::Server::bind(&"0.0.0.0:8000".parse().unwrap());
+    // // Run it with an ngrok tunnel instead!
+    axum::Server::builder(tun).serve(app.into_make_service_with_connect_info::<SocketAddr>());
+
+    // axum::Server::builder(start_tunnel().await?)
+    //     .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+    //     .await
+    //     .unwrap();
+
+    Ok(())
+}
